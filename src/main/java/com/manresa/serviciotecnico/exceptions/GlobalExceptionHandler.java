52b27f82f023db0aa95032dace80b71f1a3638c5
Adriana Manresa <adrianabmanresa@gmail.com>
2026-07-17 11:35:16 -0300

    feat: agrega validaciones con @NotBlank y manejo de errores 400

--- src/main/java/com/manresa/serviciotecnico/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/manresa/serviciotecnico/exceptions/GlobalExceptionHandler.java
@@ -2,20 +2,32 @@ package com.manresa.serviciotecnico.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<String> manejarNoEncontrado(NoSuchElementException ex) {
-
-        // respuesta linda
         String mensajeAmigable = "Error: El registro que estás buscando no existe en la base de datos.";
-
-        // Devolvemos el mensaje con el código 404 (NOT_FOUND) en vez del feo 500
         return new ResponseEntity<>(mensajeAmigable, HttpStatus.NOT_FOUND);
+    }
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> manejarValidaciones(MethodArgumentNotValidException ex) {
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errores.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
     }
 }
